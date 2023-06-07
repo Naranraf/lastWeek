2877986be469d58ae28f29ace9322716ed530feb
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-06-07 14:34:29 -0600

    1

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import { Card } from 'react-bootstrap';
 
 
 const ProductDetail = () => {
     const { id } = useParams();
     const [product, setProduct] = useState({});
-    const [rate, setRate] = useState({})
+    const [rate, setRate] = useState(0)
     const decrement = () => {
         if (rate > 1) {
             setRate(rate - 1)
@@ -20,25 +21,47 @@ const ProductDetail = () => {
         axios
             .get(`https://e-commerce-api-v2.academlo.tech/api/v1/products/1`)
             .then((resp) => {
-                setProduct(resp.data.category.id);
-                console.log(resp.data);
+                setProduct(resp.data);
             })
             .catch((err) => console.log(err));
     }, [id]);
 
+
+
+
     return (
         <div>
-            <h1>{product.title}</h1>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <Button
-                variant="secondary"
-                onClick={() => decrement()(rate - 1)}
-            >-</Button>
-            <h4>numero</h4>
-            <Button variant="secondary"
-                onClick={() => setRate(rate + 1)}
-            >+</Button>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={product.img} />
+                <Card.Body>
+                    <Card.Title>
+                        {product.title}
+                    </Card.Title>
+                    <Card.Text>
+                        {product.description}
+                    </Card.Text>
+                    <Card.Text>
+                        {product.price}
+                    </Card.Text>
+                    <Button
+                        variant="secondary"
+                        onClick={() => decrement()(rate - 1)}
+                    >-
+                    
+                    </Button>
+                    <h4>{rate}</h4>
+                    <Button variant="secondary"
+                        onClick={() => setRate(rate + 1)}
+                    >+
+                    </Button>
+                    
+                </Card.Body>
+            </Card>
+
+
+
+
+
         </div>
     );
 };
