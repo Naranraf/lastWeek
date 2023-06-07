c5229152ac0444f2a972b9d96a44dcdc49e5df6c
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-06-06 19:01:02 -0600

    7

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const ProductDetail = () => {
+    const { id } = useParams()
+    const [product, setProduct] = useState({})
+    useEffect(() => {
+        axios
+            .get(`https://e-commerce-api-v2.academlo.tech/api/v1/products/${id}`)
+            .then(resp => { console.log(resp.data) })
+            .catch(err => console.log(err))
+        setProduct(resp.data)
+
+    })
+
+
+
+
+
     return (
         <div>
-            
+            <h1>{product.title}</h1>
         </div>
     );
 };
