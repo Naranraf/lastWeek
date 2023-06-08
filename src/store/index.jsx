0a4e6a7ea9b8623a6d0782be5781aa2884698996
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-06-07 22:02:45 -0600

    sedrys

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import isLoading from '/slices/isLoading.slice';
-import productReducer from '/slices/products.slice'; // Cambiar el nombre del import
+import productReducer from '/slices/products.slice';
 
 export default configureStore({
   reducer: {
     isLoading,
-    products: productReducer, // Cambiar el nombre del reducer
+    products: productReducer
   },
 });
