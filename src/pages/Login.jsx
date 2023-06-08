005d9923bba216b633a6a3f4d7789b9009cdfce9
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-06-07 20:35:45 -0600

    5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,48 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+
+
 
 const Login = () => {
+const {register, handleSubmit} = useForm()
+
+const submit =(data) => {
+    console.log(data)
+}
+
+
     return (
         <div>
             <h1>Login</h1>
+            <Form onSubmit={handleSubmit(submit)}  border="primary" className='p-3'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        {...register("email")}
+                    />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        {...register("password")}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
