57df195f0327f24fe63f28683036b63fb8a04508
Naranraf <106176753+Naranraf@users.noreply.github.com>
2023-06-07 20:47:16 -0600

    Update index.jsx

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import isLoading from './slices/isLoading.slice';
-import productReducer from './slices/products.slice'; // Cambiar el nombre del import
+import isLoading from '/slices/isLoading.slice';
+import productReducer from '/slices/products.slice'; // Cambiar el nombre del import
 
 export default configureStore({
   reducer: {
