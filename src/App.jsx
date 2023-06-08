5d21e9f9ddb170882bcb1ac018993ef47a2fc547
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-06-07 22:28:24 -0600

    sertg

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import AppNavbar from "/components/AppNavBar"
 import Home from "/pages/Home";
 import Favorites from "/pages/Favorites";
 import Login from "/pages/Login";
-import ProductDetail from '/pages/ProductDetail';
-import Loader from '/components/Loader';
+import ProductDetail from "./pages/p";
+import Loader from "./components/Loader";
 import {useDispatch, useSelector } from "react-redux";
-import Container from 'react-bootstrap/Container';
+import { Container } from "react-bootstrap";
 import { HashRouter,Routes, Route } from "react-router-dom";
+import {Loader} from "./components/Loader"
+import { Form } from 'react-bootstrap/Form';
+import { ProductDetail } from '/pages/ProductDetail';
 
 
 function App() {
