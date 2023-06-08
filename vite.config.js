56e74488faaa2c241ae64db25992e34802464ea9
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-06-07 21:46:06 -0600

    897

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       external: [
         'bootswatch/dist/lux/bootstrap.min.css',
-        '/App'
+        '/App','/store',
       ],
     },
   },
