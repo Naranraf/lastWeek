e88fcb662ee14391a3572d5f7d3efcfe9ab8f0a8
Naranraf <106176753+Naranraf@users.noreply.github.com>
2023-06-07 20:45:45 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import AppNavbar from "./components/AppNavBar"
-import Home from "./pages/Home";
-import Favorites from "./pages/Favorites";
-import Login from "./pages/Login";
-import ProductDetail from './pages/ProductDetail';
-import Loader from './components/Loader';
+import AppNavbar from "/components/AppNavBar"
+import Home from "/pages/Home";
+import Favorites from "/pages/Favorites";
+import Login from "/pages/Login";
+import ProductDetail from '/pages/ProductDetail';
+import Loader from '/components/Loader';
 import {useDispatch, useSelector } from "react-redux";
 import Container from 'react-bootstrap/Container';
 import stylesheet from './App.css'
